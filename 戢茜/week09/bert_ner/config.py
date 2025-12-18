f07cb47f13544fed28afbf6607154b0f483b66ca
SeeingFM <sailor.1989@163.com>
2025-12-18 21:30:31 +0800

    Create config.py

---
+++ 戢茜/week09/bert_ner/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "model_output",
+    "schema_path": r"D:\BaiduNetdiskDownload\第九周 序列标注\week9 序列标注问题\week9 序列标注问题\ner\ner_data/schema.json",
+    "train_data_path": r"D:\BaiduNetdiskDownload\第九周 序列标注\week9 序列标注问题\week9 序列标注问题\ner\ner_data/train",
+    "valid_data_path": r"D:\BaiduNetdiskDownload\第九周 序列标注\week9 序列标注问题\week9 序列标注问题\ner\ner_data/test",
+    "vocab_path":"chars.txt",
+    "max_length": 100,
+    "hidden_size": 384,
+    "num_layers": 2,
+    "epoch": 20,
+    "batch_size": 16,
+    "optimizer": "adam",
+    "learning_rate": 0.001,
+    "use_crf": True,
+    "class_num": 9,
+    "bert_path": r"D:\BaiduNetdiskDownload\第六周 语言模型\bert-base-chinese\bert-base-chinese"
+}
+
